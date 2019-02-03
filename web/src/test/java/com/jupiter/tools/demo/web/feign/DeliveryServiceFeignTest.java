7962e0f1be6aaf6f31b03a16845ed70cf44ea357
Anatoliy Korovin <antkorwin@gmail.com>
2019-02-04 08:43:39 +1000

    custom server list

--- web/src/test/java/com/jupiter/tools/demo/web/feign/DeliveryServiceFeignTest.java
+++ web/src/test/java/com/jupiter/tools/demo/web/feign/DeliveryServiceFeignTest.java
@@ -1,11 +1,21 @@
 package com.jupiter.tools.demo.web.feign;
 
-import com.jupiter.tools.demo.web.feign.DeliveryServiceFeign;
 import com.jupiter.tools.spring.test.web.annotation.EnableEmbeddedWebServerTest;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractServerList;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,13 +38,38 @@ class DeliveryServiceFeignTest {
     @TestConfiguration
     public static class TestCfg {
 
+        @Bean
+        public ServerList<Server> customServerList() {
+            return new CustomServerList();
+        }
+
         @RestController
         @RequestMapping("/delivery")
         public class DeliveryApi {
 
             @PostMapping("/send")
-            public void send(@RequestParam(name = "text") String text){
+            public void send(@RequestParam(name = "text") String text) {
                 assertThat(text).isEqualTo("123");
+            }
+        }
+
+        public class CustomServerList extends AbstractServerList<Server> {
+
+            private IClientConfig clientConfig;
+
+            @Override
+            public List<Server> getInitialListOfServers() {
+                return getUpdatedListOfServers();
+            }
+
+            @Override
+            public List<Server> getUpdatedListOfServers() {
+                return Arrays.asList(new Server("127.0.0.1", Integer.valueOf(System.getProperty("server.port"))));
+            }
+
+            @Override
+            public void initWithNiwsConfig(IClientConfig clientConfig) {
+                this.clientConfig = clientConfig;
             }
         }
     }
--- web/src/test/java/com/jupiter/tools/demo/web/feign/TemplateServiceFeignTest.java
+++ web/src/test/java/com/jupiter/tools/demo/web/feign/TemplateServiceFeignTest.java
@@ -1,13 +1,21 @@
 package com.jupiter.tools.demo.web.feign;
 
 import com.jupiter.tools.spring.test.web.annotation.EnableEmbeddedWebServerTest;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractServerList;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,6 +39,11 @@ class TemplateServiceFeignTest {
     @TestConfiguration
     public static class TestCfg {
 
+        @Bean
+        public ServerList<Server> customServerList() {
+            return new TestCfg.CustomServerList();
+        }
+
         @RestController
         @RequestMapping("/templates")
         public class TemplatesApi {
@@ -39,6 +52,26 @@ class TemplateServiceFeignTest {
             public String make(@RequestParam("username") String username,
                                @RequestParam("balance") int balance) {
                 return String.format("%s balance = %d", username, balance);
+            }
+        }
+
+        public class CustomServerList extends AbstractServerList<Server> {
+
+            private IClientConfig clientConfig;
+
+            @Override
+            public List<Server> getInitialListOfServers() {
+                return getUpdatedListOfServers();
+            }
+
+            @Override
+            public List<Server> getUpdatedListOfServers() {
+                return Arrays.asList(new Server("127.0.0.1", Integer.valueOf(System.getProperty("server.port"))));
+            }
+
+            @Override
+            public void initWithNiwsConfig(IClientConfig clientConfig) {
+                this.clientConfig = clientConfig;
             }
         }
     }
